fc5ea634faa12fae84607a9a33b36e3687eb413b
Mariam Sood <mariamsood@Mariams-MacBook-Pro.local>
2018-02-26 00:23:08 +0000

    basic getting around commands

---
+++ basic-help.R
@@ -0,0 +1,40 @@
+#***Getting around***
+
+#get help when you know the function name
+?data.frame
+
+#get help when you know the subject area
+help.search("data input")
+
+#find which package the function is in, the package must be loaded
+find("glm")
+
+#find all possible items which atleast partially matches the search item
+apropos("lm")
+
+#worked examples of functions
+example("lm")
+
+#demo of R code
+demo(graphics)
+
+#use a package
+library(glmnet)
+
+#contents of a package
+library(help="glmnet")
+
+#install package
+install.packages("car")
+
+#to see variables created in a session
+objects()
+
+#to see which packages are currently attached
+search()
+
+#remove an object called 'a'
+rm(a)
+
+#remove all objects
+rm(list=ls())
